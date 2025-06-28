ebeb3fe5dde494e6e828268d079d4ccdb0341608
Luka Zukhbaia <lzukhbaia@velitech.com>
2025-06-28 19:52:18 +0400

    fet

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ class_names = os.listdir('compressed_dataset/')
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model, preprocess = clip.load("ViT-B/32", device=device, jit=False)
 
-model.load_state_dict(torch.load("models/20240429-134354_model.pt", map_location=device))
+model.load_state_dict(torch.load("models/20250628-183437_model.pt", map_location=device))
 model = model.to(device)
 
 model.eval()
@@ -29,26 +29,30 @@ text_inputs = torch.cat([clip.tokenize(f'{c}') for c in class_names]).to(device)
 print('model is ready! Press the n key to take a screenshot and feed it into the model! Press any other button to close (crash) this program')
 
 def on_press(key):
-    if key.char == 'n':
-        print("\nScreenshotted! Feeding image into model...")
-        image = pyautogui.screenshot()
-        image = crop_center(image, 1536, 662)
-        image.save('cropped.png') # use this if you want to see the screenshot that is being fed into the model
-        image_input = preprocess(image).unsqueeze(0).to(device)
+    try:
 
-        with torch.no_grad():
-            image_features = model.encode_image(image_input)
-            text_features = model.encode_text(text_inputs)
+        if key.char == 'n':
+            print("\nScreenshotted! Feeding image into model...")
+            image = pyautogui.screenshot()
+            image = crop_center(image, 1536, 662)
+            image.save('cropped.png') # use this if you want to see the screenshot that is being fed into the model
+            image_input = preprocess(image).unsqueeze(0).to(device)
 
-        image_features /= image_features.norm(dim=-1, keepdim=True)
-        text_features /= text_features.norm(dim=-1, keepdim=True)
-        similarity = (100.0 * image_features @ text_features.T).softmax(dim=-1)
-        values, indices = similarity[0].topk(5)
+            with torch.no_grad():
+                image_features = model.encode_image(image_input)
+                text_features = model.encode_text(text_inputs)
 
-        print("Top prediction(s):")
-        for value, index in zip(values, indices):
-            print(f"{class_names[index]}: {100*value.item():.2f}%")
-    else:
+            image_features /= image_features.norm(dim=-1, keepdim=True)
+            text_features /= text_features.norm(dim=-1, keepdim=True)
+            similarity = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+            values, indices = similarity[0].topk(5)
+
+            print("Top prediction(s):")
+            for value, index in zip(values, indices):
+                print(f"{class_names[index]}: {100*value.item():.2f}%")
+        else:
+            return False
+    except:
         return False
 
 # Collect events until released
